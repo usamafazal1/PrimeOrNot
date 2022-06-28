45b08a36be64d8ac6ddd9373ec82c07a7862108f
Usama Fazal <Usama Fazal@WIN-VOL3D49H6UK>
2022-06-29 00:45:30 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace QuestionNo8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            // take input and store it in variable called 'number'
+            Console.WriteLine("Input your number to check if it is prime or not: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+     
+
+            //test for positive number
+            int a = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number%i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} is a prime number", number);
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Not a prime");
+            }
+        }
+    }
+}
